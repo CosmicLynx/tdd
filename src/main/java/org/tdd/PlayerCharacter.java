64cda92e77d9504e9468b98050606d34ec55ad45
Lynx <lynx@wiltner.net>
2025-03-27 13:59:01 +0100

    implement small game + tests

--- src/main/java/org/tdd/PlayerCharacter.java
+++ src/main/java/org/tdd/PlayerCharacter.java
@@ -1,7 +1,60 @@
 package org.tdd;
 
+import java.util.Scanner;
+
 public class PlayerCharacter {
-    public static int getX() {
-        return 0;
+    public static void main(String[] args) {
+
+        int x = 0;
+        int y = 0;
+
+        boolean isRunning = true;
+        Scanner in = new Scanner(System.in);
+
+        while (isRunning) {
+            System.out.println("current location: " + "[" + x + "," + y + "]");
+            System.out.println("enter q to quit");
+            System.out.println("move with w, a, s, d: ");
+            String input = in.nextLine();
+
+            int[] newLocation = move(input.toLowerCase(), x, y);
+            x = getX(newLocation);
+            y = getY(newLocation);
+
+            isRunning = !input.equalsIgnoreCase("q");
+            if (!isRunning)
+                in.close();
+        }
+
+    }
+
+    public static int getX(int[] location) {
+        return location[0];
+    }
+
+    public static int getY(int[] location) {
+        return location[1];
+    }
+
+    public static int[] move(String input, int currentX, int currentY) {
+
+        switch (input) {
+            case "w":
+                currentY = currentY + 1;
+                break;
+            case "a":
+                currentX = currentX - 1;
+                break;
+            case "s":
+                currentY = currentY - 1;
+                break;
+            case "d":
+                currentX = currentX + 1;
+                break;
+        }
+
+        int[] newLocation = {currentX, currentY};
+
+        return newLocation;
     }
 }
--- src/test/java/org/tdd/MainTest.java
+++ src/test/java/org/tdd/MainTest.java
@@ -1,11 +1,43 @@
 package org.tdd;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
     @Test
     void testGetXReturns0() {
-        assertTrue(PlayerCharacter.getX() == 0);
+        int[] location = {0, 0};
+        assertTrue(PlayerCharacter.getX(location) == 0);
+    }
+
+    @Test
+    void testGetYReturns0() {
+        int[] location = {0, 0};
+        assertTrue(PlayerCharacter.getY(location) == 0);
+    }
+
+    @Test
+    void testMoveWMovesUp() {
+        int[] testArr = {0, 1};
+        assertTrue(Arrays.equals(PlayerCharacter.move("w", 0, 0), testArr));
+    }
+
+    @Test
+    void testMoveAMovesLeft() {
+        int[] testArr = {-1, 0};
+        assertTrue(Arrays.equals(PlayerCharacter.move("a", 0, 0), testArr));
+    }
+
+    @Test
+    void testMoveSMovesDown() {
+        int[] testArr = {0, -1};
+        assertTrue(Arrays.equals(PlayerCharacter.move("s", 0, 0), testArr));
+    }
+
+    @Test
+    void testMoveDMovesRight() {
+        int[] testArr = {1, 0};
+        assertTrue(Arrays.equals(PlayerCharacter.move("d", 0, 0), testArr));
     }
 }
